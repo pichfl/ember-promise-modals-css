1a2838111a0937121b7bf7c6bf5f3e94cafd41d2
Florian Pichler <fp@ylk.gd>
2021-01-14 18:04:00 +0100

    Update dummy application with modal examples

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,12 +1,14 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   modals: service(),
 
-  actions: {
-    showModal() {
-      this.modals.open('modal1');
-    },
-  },
+  flip: false,
+
+  showModal: action(function (options) {
+    this.modals.open('modal1', undefined, options);
+    this.toggleProperty('flip');
+  }),
 });
